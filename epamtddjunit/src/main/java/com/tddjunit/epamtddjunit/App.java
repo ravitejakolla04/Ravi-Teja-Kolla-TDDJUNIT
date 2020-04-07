324cbb44d8df19660251afdfece3313b8b5843a3
ravitejakolla04 <40908100+ravitejakolla04@users.noreply.github.com>
2020-04-07 21:28:54 +0530

    Add files via upload

---
+++ epamtddjunit/src/main/java/com/tddjunit/epamtddjunit/App.java
@@ -0,0 +1,14 @@
+package com.tddjunit.epamtddjunit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
+
---
+++ epamtddjunit/src/test/java/com/tddjunit/epamtddjunit/AppTest.java
@@ -0,0 +1,20 @@
+package com.tddjunit.epamtddjunit;
+//import static org.junit.Assert.assertTrue;
+//import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
